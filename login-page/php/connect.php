dbee279ce3efd82b87391313ba96cf893ae452b7
Aleksander Okon <aleksander.okon@medicover.pl>
2022-07-08 14:51:08 +0200

    update compose file

--- login-page/php/connect.php
+++ login-page/php/connect.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
 $db_user = "root";
-$db_password = "";
+$db_password = "admin";
 $db_name = "logowanie";
 
 // $polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
