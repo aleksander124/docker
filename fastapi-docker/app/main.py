6ffda4d0e58a413c8edeaaaad63a99cc0412e606
Aleksander OkoÅn <olerandox@gmail.com>
2022-07-22 15:21:09 +0200

    change env

--- fastapi-docker/app/main.py
+++ fastapi-docker/app/main.py
@@ -1,16 +1,18 @@
-from typing import Optional
+from typing import Union
 from fastapi import FastAPI
 import uvicorn
 
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
+def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
