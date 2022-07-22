e4b12db13e0a08724a31b1f0b6bcd2c319113b07
Aleksander OkoÅn <olerandox@gmail.com>
2022-07-22 15:03:29 +0200

    add initial if in main

--- fastapi-docker/app/main.py
+++ fastapi-docker/app/main.py
@@ -1,5 +1,7 @@
 from typing import Optional
 from fastapi import FastAPI
+import uvicorn
+
 
 app = FastAPI()
 
@@ -10,3 +12,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app)
